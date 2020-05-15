0913e7cf4eac83bacbd38258e7d132b41a6b1bd4
Neeb <thomas.neeb@nttdata.com>
2020-05-15 10:55:41 +0200

    Initial checkin

---
+++ src/main/java/de/neebs/MyMojo.java
@@ -0,0 +1,40 @@
+package de.neebs;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Goal which touches a timestamp file.
+ */
+@Mojo( name = "mixer", defaultPhase = LifecyclePhase.GENERATE_SOURCES )
+public class MyMojo
+    extends AbstractMojo
+{
+    /**
+     * Location of the file.
+     */
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputFile", required = true )
+    private String outputFile;
+
+    @Parameter( property = "inputFile", required = true)
+    private String inputFile;
+
+    public void execute() throws MojoExecutionException {
+        try {
+            SwaggerMixer swaggerMixer = new SwaggerMixer();
+            swaggerMixer.run(new ObjectMapper(), inputFile, outputFile);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
